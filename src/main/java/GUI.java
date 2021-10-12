0d8fbbe8f52cedc49dcc8d204b8e0051df82bd1c
tillmann <tillmannwillett9@gmail.com>
2021-10-12 09:55:52 +0100

    12/10

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,2 +1,40 @@
-public class GUI {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    private String[] components = {"1", "2", "3", "4", "5"};
+    private JList list;
+    private JLabel label;
+    private JButton button;
+
+    public GUI() {
+        setLayout(new FlowLayout());
+        label = new JLabel("List of components:");
+        add(label);
+        list = new JList(components);
+        add(list);
+        button = new JButton("Add component");
+        add(button);
+
+        event e = new event();
+
+        button.addActionListener(e);
+    }
+
+    public class event implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            InputWindow input = new InputWindow();
+            input.setVisible(true);
+            input.setSize(300, 150);
+        }
+    }
+
+    public static void main(String args[]) {
+        GUI gui = new GUI();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.setSize(300, 150);
+        gui.setTitle("GUI");
+    }
 }
---
+++ src/main/java/InputWindow.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InputWindow extends JFrame {
+    private JButton button;
+    private JTextField sourceTextfield, destTextfield, IdTextfield;
+    private JLabel sourcePromptlabel, destPromptlabel, IdPromptLabel;
+
+    public InputWindow() {
+        setLayout(new FlowLayout());
+        IdPromptLabel = new JLabel("Enter the component's ID");
+        add(IdPromptLabel);
+        IdTextfield = new JTextField(5);
+        add(IdTextfield);
+        sourcePromptlabel = new JLabel("Enter the source node");
+        add(sourcePromptlabel);
+        sourceTextfield = new JTextField(5);
+        add(sourceTextfield);
+        destPromptlabel = new JLabel("Enter the destination node");
+        add(destPromptlabel);
+        destTextfield = new JTextField(5);
+        add(destTextfield);
+        button = new JButton("Enter");
+        add(button);
+        event e = new event();
+        button.addActionListener(e);
+    }
+    public class event implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int sourceNode = (int) (Double.parseDouble(sourceTextfield.getText()));
+            int destNode = (int) (Double.parseDouble(sourceTextfield.getText()));
+            String ID = (sourceTextfield.getText());
+            dispose();
+        }
+    }
+}
